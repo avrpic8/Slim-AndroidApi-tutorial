51396cd0baf7925454bae0ef06f5eb0a18051e46
saeed <avrpic8@gmail.com>
2021-01-31 11:49:24 +0330

    add TestActivity to project

---
+++ app/src/main/java/com/smartElectronics/slim/activities/TestActivity.java
@@ -0,0 +1,16 @@
+package com.smartElectronics.slim.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smartElectronics.slim.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
